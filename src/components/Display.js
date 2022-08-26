f217a877a854a85fe0d19bd21829c3aea127a009
Maciej Sroka <sroka.maciej93@gmail.com>
2022-08-26 11:26:37 +0200

    Add Display and Form components

---
+++ src/components/Display.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Display extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div className="display"></div>;
+  }
+}
+
+export default Display;
---
+++ src/components/Form.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Form extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <form className="form"></form>;
+  }
+}
+
+export default Form;
