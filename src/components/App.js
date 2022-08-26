a19ca686c74512d2f6e6fd9763bce040202a6e68
Maciej Sroka <sroka.maciej93@gmail.com>
2022-08-26 14:20:20 +0200

    Initialize state for General component inputs

--- src/components/App.js
+++ src/components/App.js
@@ -6,14 +6,21 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      form: {},
+      form: {
+        name: 'John Doe',
+        title: 'JavaScript Developer',
+        location: 'Kraków, PL',
+        phone: '1235678995',
+        email: 'example@google.com',
+        website: 'https://example.com',
+      },
     };
   }
 
   render() {
     return (
       <div className="app">
-        <Form />
+        <Form info={this.state.form} />
         <Display />
       </div>
     );
