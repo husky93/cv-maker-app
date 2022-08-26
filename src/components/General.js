b783a0270dba2341a94e66c360ec285c25e4caaf
Maciej Sroka <sroka.maciej93@gmail.com>
2022-08-26 14:14:51 +0200

    Create General component initial template

--- src/components/General.js
+++ src/components/General.js
@@ -9,10 +9,46 @@ class General extends Component {
   render() {
     return (
       <div className="input-group form-general">
+        <h2 className="group-heading">General Information</h2>
         <Input
           type="text"
           className="input--name"
           placeholder="Name"
+          value="placeholder value"
+          handleInputChange={() => {}}
+        />
+        <Input
+          type="text"
+          className="input--title"
+          placeholder="Title"
+          value="placeholder value"
+          handleInputChange={() => {}}
+        />
+        <Input
+          type="text"
+          className="input--location"
+          placeholder="Location"
+          value="placeholder value"
+          handleInputChange={() => {}}
+        />
+        <Input
+          type="tel"
+          className="input--phone"
+          placeholder="Phone number"
+          value="placeholder value"
+          handleInputChange={() => {}}
+        />
+        <Input
+          type="email"
+          className="input--email"
+          placeholder="E-mail address"
+          value="placeholder value"
+          handleInputChange={() => {}}
+        />
+        <Input
+          type="url"
+          className="input--website"
+          placeholder="https://example.com"
           value="placeholder value"
           handleInputChange={() => {}}
         />
