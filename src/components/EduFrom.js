a2b0eff40128549fe987ae190d5ec2b573bb794a
Maciej Sroka <sroka.maciej93@gmail.com>
2022-08-26 16:54:26 +0200

    Create EduForm component

---
+++ src/components/EduFrom.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import Button from './Button';
+import Input from './Input';
+
+class EduForm extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const { info, id, handleInputChange, handleFormDelete } = this.props;
+    return (
+      <div className="input-group form-experience" data-id={id}>
+        <Input
+          type="text"
+          name="school"
+          className="input--school"
+          placeholder="School"
+          value={info.school}
+          handleInputChange={handleInputChange}
+        />
+        <Input
+          type="text"
+          name="degree"
+          className="input--degree"
+          placeholder="Degree"
+          value={info.degree}
+          handleInputChange={handleInputChange}
+        />
+        <Input
+          type="text"
+          name="date"
+          className="input--date"
+          placeholder="Date from - Date to"
+          value={info.date}
+          handleInputChange={handleInputChange}
+        />
+        <Input
+          type="text"
+          name="location"
+          className="input--location"
+          placeholder="Location"
+          value={info.location}
+          handleInputChange={handleInputChange}
+        />
+        <Button
+          text="Delete"
+          handleClick={handleFormDelete}
+          className="btn--danger"
+        />
+      </div>
+    );
+  }
+}
+
+export default EduForm;
