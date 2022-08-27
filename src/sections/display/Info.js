8c86b52de02254ce2e2bc68f4df8cf926a611a03
Maciej Sroka <sroka.maciej93@gmail.com>
2022-08-27 16:07:44 +0200

    Display Education and Experience

--- src/sections/display/Info.js
+++ src/sections/display/Info.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import Heading from '../../components/Heading';
+import Text from '../../components/Text';
 
 class Info extends Component {
   constructor(props) {
@@ -7,7 +9,62 @@ class Info extends Component {
 
   render() {
     const { info } = this.props;
-    return <div className="display-info"></div>;
+    return (
+      <div className="display-info">
+        <div className="display-edu">
+          <Heading className="main-heading" tag="h2" text="Education" />
+          {info.education.map((item, index) => {
+            return (
+              <div className="education-group" key={`edu-display-${index}`}>
+                <Heading tag="h3" className="heading--mid" text={item.degree} />
+                <Text tag="strong" className="text--title" text={item.school} />
+                <div className="education-info">
+                  <Text tag="span" className="text--cursive" text={item.date} />
+                  <Text
+                    tag="span"
+                    className="text--cursive"
+                    text={item.location}
+                  />
+                </div>
+                <Text
+                  tag="p"
+                  className="text--description"
+                  text={item.description}
+                />
+              </div>
+            );
+          })}
+        </div>
+        <div className="display-exp">
+          <Heading className="main-heading" tag="h2" text="Experience" />
+          {info.experience.map((item, index) => {
+            return (
+              <div className="experience-group" key={`exp-display-${index}`}>
+                <Heading tag="h3" className="heading--mid" text={item.title} />
+                <Text
+                  tag="strong"
+                  className="text--title"
+                  text={item.company}
+                />
+                <div className="education-info">
+                  <Text tag="span" className="text--cursive" text={item.date} />
+                  <Text
+                    tag="span"
+                    className="text--cursive"
+                    text={item.location}
+                  />
+                </div>
+                <Text
+                  tag="p"
+                  className="text--description"
+                  text={item.description}
+                />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
   }
 }
 
