0c4566e755756339c72accb39748fbc1f603b0e1
Maciej Sroka <sroka.maciej93@gmail.com>
2022-08-27 14:44:34 +0200

    Add all elements to Header bottom section

--- src/sections/display/Header.js
+++ src/sections/display/Header.js
@@ -3,6 +3,10 @@ import Heading from '../../components/Heading';
 import Text from '../../components/Text';
 import IconGroup from '../../components/IconGroup';
 import { ReactComponent as MailIcon } from '../../assets/icons/mail.svg';
+import { ReactComponent as GithubIcon } from '../../assets/icons/github.svg';
+import { ReactComponent as WebIcon } from '../../assets/icons/globe.svg';
+import { ReactComponent as PinIcon } from '../../assets/icons/map-pin.svg';
+import { ReactComponent as PhoneIcon } from '../../assets/icons/smartphone.svg';
 
 class Header extends Component {
   constructor(props) {
@@ -21,6 +25,18 @@ class Header extends Component {
         <div className="header-bottom">
           <IconGroup text={info.email}>
             <MailIcon />
+          </IconGroup>
+          <IconGroup text={info.phone}>
+            <PhoneIcon />
+          </IconGroup>
+          <IconGroup text={info.location}>
+            <PinIcon />
+          </IconGroup>
+          <IconGroup text={info.website}>
+            <WebIcon />
+          </IconGroup>
+          <IconGroup text={info.github}>
+            <GithubIcon />
           </IconGroup>
         </div>
       </div>
