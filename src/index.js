846d71a5fd744d5c7cb6e25906e34f2a6df8eb5a
Maciej Sroka <sroka.maciej93@gmail.com>
2022-08-27 17:06:42 +0200

    Style header top

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Main from './layouts/Main';
 import reportWebVitals from './reportWebVitals';
+import './assets/styles/reset.css';
 import './assets/styles/index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
